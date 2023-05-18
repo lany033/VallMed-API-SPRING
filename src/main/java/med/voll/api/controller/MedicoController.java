c7e727ecc5371c405b3b6a24916b5ee92f8c391f
Melanie <mmantilla930@gmail.com>
2023-05-17 20:39:19 -0500

    modelo-conexionmysql-id

---
+++ src/main/java/med/voll/api/controller/MedicoController.java
@@ -0,0 +1,23 @@
+package med.voll.api.controller;
+
+import jakarta.validation.Valid;
+import med.voll.api.medico.DatosRegistroMedico;
+import med.voll.api.medico.Medico;
+import med.voll.api.medico.MedicoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/medicos")
+public class MedicoController {
+    @Autowired
+    private MedicoRepository medicoRepository;
+    @PostMapping
+    public void registrarMedico(@RequestBody @Valid DatosRegistroMedico datosRegistroMedico){
+        //@RequestBody -> indica que es el body q llega del request
+        medicoRepository.save(new Medico(datosRegistroMedico));
+    }
+}
